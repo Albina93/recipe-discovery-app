b5594c3bb297c9e531a16b10431aec6e996573dc
Albina93 <alba.tleubergen@gmail.com>
2026-05-13 00:52:09 -0400

    Add useFetch custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import Spinner from "./components/Spinner";
 import ErrorMessage from "./components/ErrorMessage";
+import useFetch from "./hooks/useFetch";
+import { API } from "./api/endpoints";
+import type { Category } from "./types";
+
 function App() {
+  const { data, loading, error } = useFetch<{ categories: Category[] }>(
+    API.categories,
+  );
+
+  if (loading) return <Spinner />;
+  if (error) return <ErrorMessage message={error} />;
+
   return (
     <div>
       <h1>Test</h1>
-      <Spinner />
-      <ErrorMessage message="Something went wrong, while loading recipes..." />
+      <ul>
+        {data?.categories.map((cat) => (
+          <li key={cat.idCategory} style={{ padding: "1rem" }}>
+            {cat.strCategory} - {cat.strCategoryDescription}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function useFetch<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await fetch(url);
+
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+export default useFetch;
